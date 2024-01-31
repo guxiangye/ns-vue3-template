7b500f50127a4a00c403b5d9a356586870482338
guxiangye <guxiangye@shengpay.com>
2024-01-31 19:07:37 +0800

    feat(123): 123

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,25 +1,26 @@
 <script setup lang="ts">
-const test1 = () => {
-  console.log("aaaaaaaaaaaaaaaaa")
-}
-test1()
+  const test1 = () => {
+    console.log('aaaaaaaaaaaaaaaaa')
+    console.log('aaaaaaaaaaaaaaaaa')
+  }
+  test1()
 </script>
 
 <template>
-<div class="home">
-  <div class="home-nav">首页</div>
-</div>
+  <div class="home">
+    <div class="home-nav">首页</div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.home {
-  &-nav {
-    height: 44px;
-    line-height: 44px;
-    text-align: center;
-    font-size: 16px;
-    color: #fff;
-    background-color: #007aff;
+  .home {
+    &-nav {
+      height: 44px;
+      line-height: 44px;
+      text-align: center;
+      font-size: 16px;
+      color: #fff;
+      background-color: #007aff;
+    }
   }
-}
 </style>
